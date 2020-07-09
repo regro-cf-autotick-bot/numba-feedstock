58b3acf0cccbca10633a0c0528d26364bdf61932
step21 <step21@devtal.de>
2020-07-09 11:52:59 +0000

    running only a slice of tests for ppc64le and aarch64

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -36,6 +36,12 @@ numba -s
 # Check test discovery works
 python -m numba.tests.test_runtests
 
-# Run the whole test suite
-echo "Running: $SEGVCATCH python -m numba.runtests -b -m $TEST_NPROCS -- $TESTS_TO_RUN"
-$SEGVCATCH python -m numba.runtests -b --exclude-tags='long_running, important' -m $TEST_NPROCS -- $TESTS_TO_RUN
+if [[ "$archstr" == 'ppc64le' ]] | [[ "$archstr" == 'aarch64' ]]; then
+	echo 'Running only a slice of tests'
+	$SEGVCATCH python -m numba.runtests -b -j "0,None,300" --exclude-tags='long_running' -m $TEST_NPROCS -- numba.tests
+# Else run the whole test suite
+else
+	echo 'Running all the tests except long_running'
+	echo "Running: $SEGVCATCH python -m numba.runtests -b -m $TEST_NPROCS -- $TESTS_TO_RUN"
+$SEGVCATCH python -m numba.runtests -b --exclude-tags='long_running' -m $TEST_NPROCS -- $TESTS_TO_RUN
+fi
